e10a9855aaaf8ef21f1af99d4b3fbf853110a644
fbrke <maslov190407@gmail.com>
2026-01-17 17:33:25 +1000

    commit1

---
+++ KR2_N_M_SurName.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int maxel(double a[][100], int strok, int k) {
+	int stroki = 0;
+	double max_element = a[0][k];
+
+	for (int g = 1; g < strok; g++) {
+		if (a[g][k] > max_element) {
+			max_element = a[g][k];
+			stroki = g;
+		}
+	}
+	return stroki + 1;
+}
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int strok, stolb, k;
+	double a[100][100];
+	cout << "Введите количество строк и столбцов в матрице: ";
+	cin >> strok >> stolb;
+
+	cout << "Ведите элементы матрицы: ";
+	for (int i = 0; i < strok; i++) {
+		for (int j = 0; j < stolb; j++) {
+			cin >> a[i][j];
+		}
+	}
+	cout << "Введите номер столбца: ";
+	cin >> k;
+	k -= 1;
+	if (k > strok || k > stolb) {
+		cout << "Столб выходит за пределы матрицы";
+		return 0;
+	}
+	int result = maxel(a, strok, k);
+	cout << "Номер наибольшего элемента k-ого столбца: " << result;
+	return 0;
+
+}
